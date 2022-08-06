c449bde2cbac93902ada846c26862f81329c97d6
DavidGBbr <davidgomesbrigido@gmail.com>
2022-08-06 13:22:32 -0300

    first commit

---
+++ componentes/TelaCatalogo/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function TelaCatalogo() {
+ return (
+   <Text>Tela Catálogo</Text>
+  );
+}
---
+++ componentes/TelaContato/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function TelaContato() {
+ return (
+   <Text>Tela Contato</Text>
+  );
+}
